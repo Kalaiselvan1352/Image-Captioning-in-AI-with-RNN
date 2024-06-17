c0b55f38be1fd3930c4ad4b3c0cdb95cccda31eb
Kalaiselvan1352 <114053197+Kalaiselvan1352@users.noreply.github.com>
2024-06-17 19:01:38 +0530

    Add files via upload

---
+++ imagecap.py
@@ -0,0 +1,126 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Input, Add
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+
+
+image_model = InceptionV3(weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-2].output
+image_features_extract_model = Model(inputs=new_input, outputs=hidden_layer)
+
+
+
+
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(299, 299))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+
+def extract_image_features(image_path):
+    image = preprocess_image(image_path)
+    feature = image_features_extract_model.predict(image, verbose=0)
+    return feature
+
+
+
+image_paths = ['path_to_image1.jpg', 'path_to_image2.jpg', ...]
+captions = ['caption1', 'caption2', ...]
+
+
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(captions)
+vocab_size = len(tokenizer.word_index) + 1
+max_length = max(len(c.split()) for c in captions)
+
+
+
+def create_sequences(tokenizer, max_length, descriptions, photos):
+    X1, X2, y = list(), list(), list()
+    for i, desc in enumerate(descriptions):
+        seq = tokenizer.texts_to_sequences([desc])[0]
+        for j in range(1, len(seq)):
+            in_seq, out_seq = seq[:j], seq[j]
+            in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+            out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+            X1.append(photos[i])
+            X2.append(in_seq)
+            y.append(out_seq)
+    return np.array(X1), np.array(X2), np.array(y)
+
+
+
+features = [extract_image_features(path) for path in image_paths]
+
+
+
+X1, X2, y = create_sequences(tokenizer, max_length, captions, features)
+
+
+
+def define_model(vocab_size, max_length):
+    inputs1 = Input(shape=(2048,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+    
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+    
+    decoder1 = Add()([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+    
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    return model
+
+
+
+model = define_model(vocab_size, max_length)
+
+
+
+checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+
+
+
+model.fit([X1, X2], y, epochs=20, verbose=2, callbacks=[checkpoint], validation_split=0.2)
+
+
+
+def generate_caption(model, tokenizer, photo, max_length):
+    in_text = 'startseq'
+    for _ in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([photo, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = tokenizer.index_word.get(yhat)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    return in_text
+
+
+
+photo = extract_image_features('path_to_new_image.jpg')
+caption = generate_caption(model, tokenizer, photo, max_length)
+print(caption)
+
